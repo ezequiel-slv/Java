dc55240d670e243367e97dbd11a9a8c737663d4b
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-27 16:19:04 -0300

    Initial commit

--- src/java/estudo/arquivos/introdução/OláMundo.java
+++ src/java/estudo/arquivos/introdução/OláMundo.java
@@ -1,6 +1,6 @@
 package java.estudo.arquivos.introdução;
 
-public class EstudandoJava {
+public class OláMundo {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
     }
