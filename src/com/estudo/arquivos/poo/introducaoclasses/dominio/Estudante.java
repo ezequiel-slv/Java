ebc496bd624d98e4f428cf38f4c62b32d086ea25
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-30 21:11:50 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/introducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package com.estudo.arquivos.poo.introducaoclasses.dominio;
+
+public class Estudante {
+    public String nome = "Ezequiel";
+    public int idade = 22;
+    public char sexo = 'M';
+}
---
+++ src/com/estudo/arquivos/poo/introducaoclasses/teste/TestePessoas.java
@@ -0,0 +1,16 @@
+package com.estudo.arquivos.poo.introducaoclasses.teste;
+
+import com.estudo.arquivos.poo.introducaoclasses.dominio.Estudante;
+
+public class TestePessoas {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.idade = 22;
+        estudante.nome = "Ezequiel";
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
