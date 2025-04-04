a5aa59190e552ae254f4ed39f2f23cb501261126
Ezequiel <ezequielsilvalima01@gmail.com>
2025-04-04 18:03:41 -0300

    commit

--- src/com/estudo/arquivos/revisao/revisaopoo/metodos/dominio/CalculadoraRev.java
+++ src/com/estudo/arquivos/revisao/revisaopoo/metodos/dominio/CalculadoraRev.java
@@ -7,4 +7,19 @@ public class CalculadoraRev {
         }
         return(num1/num2);
     }
+
+    public int divideDoisNumeros02(int num1, int num2) {
+        if (num2 != 0){
+            return(num1/num2);
+        }
+        return 0;
+    }
+
+    public void divisaoDoisNumeros(int num1, int num2){
+        if (num2 != 0){
+            System.out.println(num1/num2);
+            return;
+        }
+        System.out.println("Não existe divisão por zero");
+    }
 }
--- src/com/estudo/arquivos/revisao/revisaopoo/metodos/teste/CalculadoraTeste.java
+++ src/com/estudo/arquivos/revisao/revisaopoo/metodos/teste/CalculadoraTeste.java
@@ -5,7 +5,12 @@ import com.estudo.arquivos.revisao.revisaopoo.metodos.dominio.CalculadoraRev;
 public class CalculadoraTeste {
     public static void main(String[] args) {
         CalculadoraRev calculadora = new CalculadoraRev();
+        System.out.println("Metodo com retorno");
         int result = calculadora.divideDoisNumeros(20,4);
         System.out.println(result);
+        System.out.println(calculadora.divideDoisNumeros(40,4));
+        System.out.println("---------");
+        System.out.println("Metodo sem retorno");
+        calculadora.divisaoDoisNumeros(100,2);
     }
 }
