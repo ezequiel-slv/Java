60b1d4946e013e9dfd0f788a19aa98903f0a26cb
Ezequiel <ezequielsilvalima01@gmail.com>
2025-04-23 14:48:31 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/blocodeinicializacao/teste/TesteSistemaOperacional.java
@@ -0,0 +1,9 @@
+package com.estudo.arquivos.poo.blocodeinicializacao.teste;
+
+import com.estudo.arquivos.poo.blocodeinicializacao.dominio.SistemaOperacional;
+
+public class TesteSistemaOperacional {
+    public static void main(String[] args) {
+        SistemaOperacional.sistema();
+    }
+}
