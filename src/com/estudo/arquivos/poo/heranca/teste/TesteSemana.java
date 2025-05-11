87df89b7606343d19bc72e69d1375b4fb1fe8350
Ezequiel <ezequielsilvalima01@gmail.com>
2025-05-10 22:41:31 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/heranca/teste/TesteSemana.java
@@ -0,0 +1,20 @@
+package com.estudo.arquivos.poo.heranca.teste;
+
+import com.estudo.arquivos.poo.heranca.dominio.Semana;
+
+public class TesteSemana {
+    public static void main(String[] args) {
+        Semana semana1 = new Semana("Segunda");
+        Semana semana2 = new Semana("Segunda");
+        Semana semana3 = new Semana("Segunda");
+        Semana semana4 = new Semana("Segunda");
+        Semana semana5 = new Semana("Segunda");
+        Semana semana6 = new Semana("Segunda");
+        Semana semana7 = new Semana("Segunda");
+        Semana[] semanas = {semana1, semana1, semana2, semana3, semana4, semana5, semana6, semana7};
+        for (Semana semana : semanas) {
+            semana.imprime();
+        }
+
+    }
+}
