f0137b729e970f32a437d086382eafcd21deb82b
Ezequiel <ezequielsilvalima01@gmail.com>
2025-06-01 21:36:43 -0300

    commit

--- src/com/estudo/arquivos/poo/exception/customizedexception/domain/Login.java
+++ src/com/estudo/arquivos/poo/exception/customizedexception/domain/Login.java
@@ -2,9 +2,6 @@ package com.estudo.arquivos.poo.exception.customizedexception.domain;
 
 public class Login extends Exception{
 
-    public Login() {
-        System.out.println("Senha ou usuário inválido");
-    }
 
     public Login(String message) {
         super(message);
