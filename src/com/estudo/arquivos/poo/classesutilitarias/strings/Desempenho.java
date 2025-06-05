24fb3576ed554d54bff9a21a4497316986ccc505
Ezequiel <ezequielsilvalima01@gmail.com>
2025-06-05 19:36:12 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/classesutilitarias/strings/Desempenho.java
@@ -0,0 +1,43 @@
+package com.estudo.arquivos.poo.classesutilitarias.strings;
+
+public class Desempenho {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        String(100_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo de execução da String base: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        StringBuider(100_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo de execução da String Builder: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        StringBuffer(100_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo de execução da String Buffer: " + (fim - inicio) + "ms");
+    }
+
+    public static void String(int tamanho){
+        String numero = "";
+        for (int i = 0; i < tamanho; i++) {
+            numero += numero;
+        }
+    }
+
+    public static void StringBuider(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+        }
+    }
+
+    public static void StringBuffer(int tamanho){
+        StringBuffer sbf = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sbf.append(i);
+        }
+    }
+
+
+}
--- src/com/estudo/arquivos/poo/classesutilitarias/strings/StringPrt01.java
+++ src/com/estudo/arquivos/poo/classesutilitarias/strings/StringPrt01.java
@@ -10,7 +10,9 @@ public class StringPrt01 {
 
         nome.concat(" Silva"); // Jeito errado
         nome = nome.concat(" Silva"); // Agora o nome vai apontar para o novo valor (nome + sobrenome)
+        String nome3 = new String("Ezequiel"); //String encapsulada
         System.out.println(nome);
         System.out.println(nome.equals(nome2));
+        System.out.println(nome == nome3.intern());
     }
 }
