84d90fe8d21607391286ed575787aa29420abd9b
Ezequiel <ezequielsilvalima01@gmail.com>
2025-06-10 21:25:29 -0300

    commit

--- src/com/estudo/arquivos/poo/classesutilitarias/date/LocaleClass.java
+++ src/com/estudo/arquivos/poo/classesutilitarias/date/LocaleClass.java
@@ -9,13 +9,18 @@ public class LocaleClass {
         Locale localeEUA = new Locale("en", "US");
         Locale localeBrazil = new Locale("pt", "BR");
         Locale localeEspanha = new Locale("es", "ES");
+        Locale localeJapao = new Locale("ja", "JP");
 
         Calendar calendar = Calendar.getInstance();
+
         DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeBrazil);
         DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeEspanha);
         DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeEUA);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapao);
+
         System.out.println("Brasil: " + df1.format(calendar.getTime()));
         System.out.println("España: " + df2.format(calendar.getTime()));
         System.out.println("United States: " + df3.format(calendar.getTime()));
+        System.out.println("日本: " + df4.format(calendar.getTime()));
     }
 }
