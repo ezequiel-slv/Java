e710ee3e8f5b8979109c6f9504c8c9cb6a8440c7
Ezequiel <ezequielsilvalima01@gmail.com>
2025-07-10 18:16:21 -0300

    commit

--- src/com/estudo/arquivos/poo/revisao/introducaometodos02/Teste/VarArgsTeste.java
+++ src/com/estudo/arquivos/poo/revisao/introducaometodos02/Teste/VarArgsTeste.java
@@ -9,5 +9,11 @@ public class VarArgsTeste {
         Array.somaArray(numeros);
 
         Array.somaVarArgs(1,2,3,4,5,6,7,8);
+
+        Array.mediaVarArgs(6.5,9,5.2);
+
+        Array.produtosValores(2, 6, 8, 2);
+
+        Array.listaNomes("Pessoa 1", "Pessoa 2", "Pessoa 3");
     }
 }
--- src/com/estudo/arquivos/poo/revisao/introducaometodos02/dominio/VarArgs.java
+++ src/com/estudo/arquivos/poo/revisao/introducaometodos02/dominio/VarArgs.java
@@ -17,4 +17,28 @@ public class VarArgs {
         }
         System.out.println("Array com VarArgs: " + soma);
     }
+
+    public void mediaVarArgs(double... notas){
+        double soma = 0;
+        for (double nota: notas){
+            soma += nota;
+        }
+
+        double media = soma / notas.length;
+        System.out.printf("Média: %.2f\n", media);
+    }
+
+    public void produtosValores(int... valores){
+        int produto = 1;
+        for (int valor: valores){
+            produto *= valor;
+        }
+        System.out.println(produto);
+    }
+
+    public void listaNomes(String... nomes){
+        for (String nome: nomes){
+            System.out.println(nome);
+        }
+    }
 }
