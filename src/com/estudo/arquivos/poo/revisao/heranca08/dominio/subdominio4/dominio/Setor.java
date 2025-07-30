d729dfca638a37ab84319b0176f7aa547af213c5
Ezequiel <ezequielsilvalima01@gmail.com>
2025-07-29 21:43:17 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/revisao/heranca08/dominio/subdominio4/dominio/Setor.java
@@ -0,0 +1,13 @@
+package com.estudo.arquivos.poo.revisao.heranca08.dominio.subdominio4.dominio;
+
+public class Setor {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/com/estudo/arquivos/poo/revisao/heranca08/dominio/subdominio4/teste/FuncionarioTeste.java
@@ -0,0 +1,14 @@
+package com.estudo.arquivos.poo.revisao.heranca08.dominio.subdominio4.teste;
+
+import com.estudo.arquivos.poo.revisao.heranca08.dominio.subdominio4.dominio.Funcionario;
+import com.estudo.arquivos.poo.revisao.heranca08.dominio.subdominio4.dominio.Setor;
+
+public class FuncionarioTeste {
+    public static void main(String[] args) {
+        Setor setor = new Setor();
+        setor.setNome("Tecnologia da Informação");
+
+        Funcionario funcionario = new Funcionario("Ezequiel", 23, 'M', "625.552.923-14", setor, 5400);
+        funcionario.imprimir();
+    }
+}
