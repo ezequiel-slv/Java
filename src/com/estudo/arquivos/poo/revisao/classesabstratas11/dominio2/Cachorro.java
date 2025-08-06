ce0f76f808df1bdcae87c7a0d06a7bcba2b3ca85
Ezequiel <ezequielsilvalima01@gmail.com>
2025-08-06 08:05:44 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/revisao/classesabstratas11/dominio2/Cachorro.java
@@ -0,0 +1,15 @@
+package com.estudo.arquivos.poo.revisao.classesabstratas11.dominio2;
+
+public class Cachorro extends Animal {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Animal: " + this.nome);
+        System.out.println("Au au");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
