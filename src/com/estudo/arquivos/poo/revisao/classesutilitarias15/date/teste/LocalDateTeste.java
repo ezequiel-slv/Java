2b0e4ab155a764a11479889c16298e1186c42c0b
Ezequiel <ezequielsilvalima01@gmail.com>
2025-09-04 19:20:13 -0300

    commit

---
+++ src/com/estudo/arquivos/poo/revisao/classesutilitarias15/date/teste/LocalDateTeste.java
@@ -0,0 +1,11 @@
+package com.estudo.arquivos.poo.revisao.classesutilitarias15.date.teste;
+
+import com.estudo.arquivos.poo.revisao.classesutilitarias15.date.dominio.LocalDateCass;
+
+public class LocalDateTeste {
+    public static void main(String[] args) {
+        LocalDateCass ld = new LocalDateCass();
+        //ld.localDate();
+        ld.localDate2();
+    }
+}
