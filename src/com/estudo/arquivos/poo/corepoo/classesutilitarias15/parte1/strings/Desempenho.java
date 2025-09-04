7aeceebe7e922845fd6b3d963f3fcddf8b578b35
Ezequiel <ezequielsilvalima01@gmail.com>
2025-09-04 10:07:04 -0300

    commit

--- src/com/estudo/arquivos/poo/corepoo/classesutilitarias15/parte1/strings/Desempenho.java
+++ src/com/estudo/arquivos/poo/corepoo/classesutilitarias15/parte1/strings/Desempenho.java
@@ -9,10 +9,15 @@ public class Desempenho {
         long fim = System.currentTimeMillis();
         System.out.println("Tempo de execução da String base: " + (fim - inicio) + "ms");
 
-        inicio = System.currentTimeMillis();
+        long inicio2 = System.currentTimeMillis();
         StringBuilder(100_000);
-        fim = System.currentTimeMillis();
-        System.out.println("Tempo de execução da String Builder: " + (fim - inicio) + "ms");
+        long fim2 = System.currentTimeMillis();
+        System.out.println("Tempo de execução da String Builder: " + (fim2 - inicio2) + "ms");
+
+        long inicio3 = System.currentTimeMillis();
+        StringBuilder(100_000);
+        long fim3 = System.currentTimeMillis();
+        System.out.println("Tempo de execução da String Buffer: " + (fim3 - inicio3) + "ms");
     }
 
     public static void String(int tamanho){
@@ -26,6 +31,13 @@ public class Desempenho {
         StringBuilder sb = new StringBuilder(tamanho);
         for (int i = 0; i < tamanho; i++) {
             sb.append(i);
+        }
+    }
+
+    public static void StringBuffer(int tamanho){
+        StringBuffer sbf = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sbf.append(i);
         }
     }
 }
