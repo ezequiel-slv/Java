3fb176dd8ac17fcc59a505ec168284edf9028a5d
Ezequiel <ezequielsilvalima01@gmail.com>
2025-09-07 21:12:40 -0300

    commit

---
+++ src/com/estudo/arquivos/classesutilitarias05/coreclassesutilitarias/parte2/instant/InstantClass.java
@@ -0,0 +1,17 @@
+package com.estudo.arquivos.classesutilitarias05.coreclassesutilitarias.parte2.instant;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantClass {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        System.out.println("---------");
+
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+
+    }
+}
