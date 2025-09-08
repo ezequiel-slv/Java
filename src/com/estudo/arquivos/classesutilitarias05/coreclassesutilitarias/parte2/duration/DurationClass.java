8ae101b1eedb00fded7f83a4fba8af5876f7fe8d
Ezequiel <ezequielsilvalima01@gmail.com>
2025-09-08 20:52:50 -0300

    commit

---
+++ src/com/estudo/arquivos/classesutilitarias05/coreclassesutilitarias/parte2/duration/DurationClass.java
@@ -0,0 +1,22 @@
+package com.estudo.arquivos.classesutilitarias05.coreclassesutilitarias.parte2.duration;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DurationClass {
+    public static void main(String[] args) {
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime afterYear = LocalDateTime.now().plusYears(2);
+        Duration between1 = Duration.between(now, afterYear);
+        System.out.println(between1);
+
+        System.out.println("-------------");
+
+        LocalTime timeNow = LocalTime.now();
+        LocalTime afterTime = LocalTime.now().plusHours(7).plusMinutes(30);
+        Duration between2 = Duration.between(timeNow, afterTime);
+        System.out.println(between2);
+    }
+}
