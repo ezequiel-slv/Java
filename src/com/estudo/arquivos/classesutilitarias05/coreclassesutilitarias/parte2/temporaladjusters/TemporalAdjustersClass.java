1189564ddfb6686f183b5c3125af9f49a9637650
Ezequiel <ezequielsilvalima01@gmail.com>
2025-09-23 22:01:00 -0300

    commit

---
+++ src/com/estudo/arquivos/classesutilitarias05/coreclassesutilitarias/parte2/temporaladjusters/TemporalAdjustersClass.java
@@ -0,0 +1,21 @@
+package com.estudo.arquivos.classesutilitarias05.coreclassesutilitarias.parte2.temporaladjusters;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersClass {
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        now = now.withDayOfMonth(23);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        System.out.println("--------");
+
+        now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.TUESDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+}
