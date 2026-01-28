e13598bd07ae08ef37c734645fa30b2152bf2047
Ezequiel <ezequielsilvalima01@gmail.com>
2026-01-27 21:41:13 -0300

    commit

---
+++ src/com/estudo/arquivos/poo03/revisao/heranca08/dominio/subdominio6/desconto.java
@@ -0,0 +1,5 @@
+package com.estudo.arquivos.poo03.revisao.heranca08.dominio.subdominio6;
+
+public interface desconto {
+    double calcularDesconto();
+}
---
+++ src/com/estudo/arquivos/poo03/revisao/heranca08/teste/PessoaTeste3.java
@@ -0,0 +1,10 @@
+package com.estudo.arquivos.poo03.revisao.heranca08.teste;
+
+import com.estudo.arquivos.poo03.revisao.heranca08.dominio.subdominio6.Funcionario;
+
+public class PessoaTeste3 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Ezequiel", 23, "Masculino", 2100);
+        funcionario.imprimir();
+    }
+}
